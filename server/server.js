d792707c1e4bc714afd64f8cf3814b221a51c858
Marthias <kasekamarthias@gmail.com>
2026-07-03 10:45:23 +0200

    Configured Express to serve frontend pages

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 require("dotenv").config();
 
 const db = require("../config/db");
@@ -9,10 +10,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Serve static files
+app.use(express.static(path.join(__dirname, "../public")));
+
 const PORT = process.env.PORT || 3000;
 
+// Load Login Page
 app.get("/", (req, res) => {
-    res.send("Security Guard Portal API is running...");
+    res.sendFile(path.join(__dirname, "../public/pages/login.html"));
 });
 
 app.listen(PORT, () => {
